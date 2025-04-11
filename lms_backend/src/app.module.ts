7849f2ccfcc7d0439ac603fe3b7cbf37b3fcfd5a
omar anas <65462442+omar-anas@users.noreply.github.com>
2025-04-11 08:53:06 +0200

    creating modules auth users tasks

--- lms_backend/src/app.module.ts
+++ lms_backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, TasksModule, UsersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
